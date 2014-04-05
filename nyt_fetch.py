95eb1437d81857df9453acf0de3a5799b67a7efe
Laura Watiker <lwatiker@gmail.com>
2014-04-05 18:59:20 -0400

    Some Temp stuff

--- nyt_fetch.py
+++ nyt_fetch.py
@@ -1,7 +1,8 @@
-import requests, json
+#!/usr/bin/python
+import requests, json, pprint
 
 def main():
-	api_key = "A5B6D9051976110FDA3D0563F0A7CC1E:6:67761527"
+	api_key = "a5b6d9051976110fda3d0563f0a7cc1e:6:67761527"
 	nyt_url = "http://api.nytimes.com/svc/news/v3/content/all/all/.json.json.json.json"
 
 	payload = {
@@ -9,8 +10,12 @@ def main():
 		'api-key': api_key
 	}
 
-	r = requests.get(nyt_url+api_key, params=payload)
-	print(r.url)
+	r = requests.get(nyt_url, params=payload)
+	content = json.loads(r.content)
+	pprint.pprint(content)
+
+	for story in content['results']:
+		print story['title']
 
 main()
 

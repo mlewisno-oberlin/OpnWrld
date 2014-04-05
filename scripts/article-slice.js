e63ab392742c051b3264e31e92ed434c3ea2d53c
mlewisno <mlewisno@gmail.com>
2014-04-05 19:18:50 -0400

    Looking at the new json file

--- scripts/article-slice.js
+++ scripts/article-slice.js
@@ -3,18 +3,8 @@ var articles = [ ];
 $(document).ready(function ($) {
     console.log(articles.length);
     $.getJSON("sample.json",    function (data) {
-        $.each( data, function( key, val ) {
-            articles.push(val);
-            var datastor = [];
-            $.each( data, function( key2, val2 ) {
-                articles.push(val);
-                var data = []
-            
-            });
-        });
+        console.log(data);
     });
-   
-
 });
 
 

c27bd06598c5a4aca4cac99070718e5958a301f0
mlewisno <mlewisno@gmail.com>
2014-04-05 20:03:45 -0400

    Fixed my JSON file parser

--- scripts/article-slice.js
+++ scripts/article-slice.js
@@ -1,9 +1,35 @@
 var articles = [ ];
 
+function iterateJSON(data) {
+    var x = 1;
+    var counter = 0;
+    
+    var arraystuff = [];
+            var title = data.title;
+            var author = data.author;
+            var location = data.location;
+            var opening = data.opening;
+            var link = data.link;
+            var pic = data.pic;
+
+            arraystuff.push(title);
+            arraystuff.push(author);
+            arraystuff.push(location);
+            arraystuff.push(opening);
+            arraystuff.push(link);
+            arraystuff.push(pic);
+            articles.push(arraystuff);
+}
+
+
 $(document).ready(function ($) {
     console.log(articles.length);
     $.getJSON("sample.json",    function (data) {
-        console.log(data);
+        console.log(data.result);
+        for(var i = 0; i < data.result.length; i++){
+            iterateJSON(data.result[i]);
+        }
+        console.log(articles);
     });
 });
 

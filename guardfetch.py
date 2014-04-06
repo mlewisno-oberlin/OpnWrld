84bb90005e03728efe5536ed87fea948b120de8b
Laura Watiker <lwatiker@gmail.com>
2014-04-05 21:02:59 -0400

    Fixed some things. Appears to be working.

--- guardfetch.py
+++ guardfetch.py
@@ -1,7 +1,7 @@
 from datetime import datetime
-import requests
-import json
-import re
+from codecs import encode
+import requests, json, re
+
 
 def getLocale(string):
     regions=["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas, The", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei ", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic of the", "Congo, Republic of the", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Curacao", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor (see Timor-Leste)", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia, The", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Holy See", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestinian Territories", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa ", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Sint Maarten", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain ", "Sri Lanka", "Sudan", "Suriname", "Swaziland ", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand ", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"]
@@ -12,7 +12,7 @@ def getLocale(string):
 
 def getOpening(string):
     if re.match('.*?/p>', string) is not None:
-        return re.match('.*?/p>', string)
+        return re.match('.*?/p>', string).group()
     else:
         return "";
 
@@ -24,16 +24,16 @@ def APIgrab():
     stories = []
     for story in content['response']['results']:
         dict = {
-            'title': story['webTitle'],
-            'author': story['fields']['byline'],
+            'title': story['webTitle'].encode('ascii', 'ignore'),
+            'author': story['fields']['byline'].encode('ascii', 'ignore'),
             'location': getLocale(story['webTitle']),
             'opening': getOpening(story['fields']['body']),
-            'link': story['webUrl'],
+            'link': story['webUrl'].encode('ascii', 'ignore'),
             'pic': re.match('".*"', story['fields']['thumbnail'])
         }
         stories.append(dict)
-        print stories
+        # print stories/
         with open('articles/guard.json', 'w') as outfile:
-            json.dump({'result': stories}, outfile, skipkeys=True, ensure_ascii=False, check_circular=False)
+            json.dump({'result': stories}, outfile, skipkeys=True, ensure_ascii=False, check_circular=False, encoding="utf-8")
     
 APIgrab()

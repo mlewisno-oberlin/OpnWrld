89846548dbf6858bb1fd7daba3173de09b166d81
Paul Buser <pbuser@oberlin.edu>
2014-04-05 23:15:31 -0400

    This is the real bubble functions

--- scripts/bubble_functions.js
+++ scripts/bubble_functions.js
@@ -13,3 +13,27 @@ function redrawBubbles() {
     //bubbles, custom popup on hover template
     map.bubbles(bubblesArray, {popupTemplate: hoverInfo});
 }
+
+ function addBubble(bubbleJSON, lat, lon) {
+            newBubble = {latitude: lat, longitude: lon, radius: bubbleSize, fillKey: 'gt50'};
+            newBubble = mergeDicts(newBubble, bubbleJSON);
+            bubblesArray.push(newBubble);
+            redrawBubbles();
+        }
+
+
+
+         // Draws a bubble at an address checked by Google Maps
+        function addBubbleAtAddress(bubbleJSON, addressString) {
+            geocoder.geocode({address: addressString}, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    lat = results[0].geometry.location.lat();
+                    lon = results[0].geometry.location.lng();
+                    locationDictionary = {location: addressString};
+                    updatedLocationDict = mergeDicts(locationDictionary, bubbleJSON);
+                    addBubble(updatedLocationDict, lat, lon);
+                } else {
+                    console.log("Geocode was not successful on address string: " + addressString + " for the following reason: " + status);
+                }
+            });
+        }
